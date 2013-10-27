0448f10f5eaa76855b2d891f341a864d183d03a2
Rui Li <kendallli@live.com>
2013-10-27 16:38:55 -0500

    kusdgqkua

--- Derek/src/com/example/project/MainActivity.java
+++ Derek/src/com/example/project/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	
+	//wqoudgfqwiu
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
