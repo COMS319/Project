54fe9c0d22c8a472cab55d9e95f1a6e276fb84f6
Derek Reiser <derek.reiser@gmail.com>
2013-10-28 10:38:31 -0500

    Now when the app starts it is in landscape mode and will stay in that mode

--- Derek/src/com/example/project/MainActivity.java
+++ Derek/src/com/example/project/MainActivity.java
@@ -2,19 +2,16 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	
-	//wqoudgfqwiu
-	//uefguidf
-	//dwiufgw
-	
-	
+		
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 
